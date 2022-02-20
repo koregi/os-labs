b92ab55e6c9bc88b4c7f09c7d75f4b2392f4ba8e
missbulochka <elina.paxar@gmail.com>
2022-02-20 16:56:38 +0300

    [OS-12] #comment Lab changes

--- src/lab_01/lab_01.cpp
+++ src/lab_01/lab_01.cpp
@@ -5,24 +5,26 @@
 bool flag1 = 0;
 bool flag2 = 0;
 
-static void * proc1(void *arg) {
+static void* proc1(void *arg) {
     printf("Thread 1 started\n");
     while (!flag1) {
         printf("1\n");
 	fflush(stdout);
 	sleep(1);
     }
-    pthread_exit((void*)3);
+    int* ret = new int(3);
+    pthread_exit(ret);
 }
 
-static void * proc2(void *arg) {
+static void* proc2(void *arg) {
     printf("Thread 2 started\n");
     while (!flag2) {
         printf("2\n");
 	fflush(stdout);
 	sleep(1);
     }
-    pthread_exit((void*)4);
+    int* ret = new int(4);
+    pthread_exit(ret);
 }
 
 int main() {
@@ -42,9 +44,9 @@ int main() {
     flag2 = 1;
 
     pthread_join(id1, (void**)&exitcode1);
-    printf("Thread 1 finished with exit code: %p\n", exitcode1);
+    printf("Thread 1 finished with exit code: %u\n", *exitcode1);
     pthread_join(id2, (void**)&exitcode2);
-    printf("Thread 2 finished with exit code: %p\n", exitcode2);
+    printf("Thread 2 finished with exit code: %u\n", *exitcode2);
 
     printf("Program finished\n");
     return 0;
