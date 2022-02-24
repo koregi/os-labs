46b09ec8209ee08837369d7e87d6a6198b6a624b
missbulochka <elina.paxar@gmail.com>
2022-02-24 21:14:43 +0300

    [OS-16] #comment Extra lab is creating

---
+++ src/lab_01/extra_task_01.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <pthread.h>
+#include <unistd.h>
+#include <cstdio>
+#include <cstdint>
+#include <errno.h>
+
+static void* proc1(void* arg) {
+    printf("Thread 1 started\n");
+    bool* flag = (bool*)arg;
+    while (!(*flag)) {
+        printf("1\n");
+        fflush(stdout);
+        sleep(1);
+    }
+    pthread_exit((void*)3);
+}
+
+static void* proc2(void* arg) {
+    printf("Thread 2 started\n");
+    bool* flag = (bool*)arg;
+    while (!(*flag)) {
+        printf("2\n");
+        fflush(stdout);
+        sleep(1);
+    }
+    pthread_exit((void*)4);
+}
+
+static void pclock(clockid_t cid) {
+    struct timespec ts;
+    printf("Clocktime value: ");
+    std::cout << clock_gettime(cid, &ts) << std::endl;
+    printf("%4ld.%03ld\n", (intmax_t)ts.tv_sec, ts.tv_nsec / 1000000);
+}
+
+
+int main() {
+    printf("Program started\n");
+
+    bool flag1 = false;
+    bool flag2 = false;
+    pthread_t id1;
+    pthread_t id2;
+    clockid_t cid1;
+    clockid_t cid2;
+    int* exitcode1;
+    int* exitcode2;
+
+    pthread_create(&id1, nullptr, proc1, &flag1);
+    pthread_getcpuclockid(id1, &cid1);
+    pthread_create(&id2, nullptr, proc2, &flag2);
+    pthread_getcpuclockid(id2, &cid2);
+
+    printf("Program is waiting for a keystroke\n");
+    getchar();
+    printf("Key pressed\n");
+    flag1 = true;
+    flag2 = true;
+
+    pthread_join(id1, (void**)&exitcode1);
+    printf("Thread 1 finished with exit code: %p\n", (void*)exitcode1);
+    pclock(cid1);
+    pthread_join(id2, (void**)&exitcode2);
+    printf("Thread 2 finished with exit code: %p\n", (void*)exitcode2);
+    pclock(cid2);
+
+    printf("Program finished\n");
+    return 0;
+}
