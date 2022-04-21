a5e036a23f2f21f27622d42bffebc1d1e94e4e10
missbulochka <elina.paxar@gmail.com>
2022-04-21 20:50:05 +0300

    [OS-37] #comment child process creating

---
+++ src/lab_04/lab_04_1.cpp
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+#include <cstdio>
+#include <cstdlib>
+
+int main(int argc, char* argv[]) {
+    printf("Child program started\n\n");
+
+    printf("This process (our child) ID of this program is %d\n", getpid());
+    printf("Parent process ID of this program is %d\n", getppid());
+
+    printf("Args are:\n");
+    for (size_t idx = 0; idx < argc; ++idx) {
+        printf("%s\n", argv[idx]);
+        sleep(1);
+    }
+
+    printf("Child program finished\n");
+    exit(3);
+}
