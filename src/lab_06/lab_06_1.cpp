ad37bc12814f11a4282e188932fb94ae70061545
bulochka <elina.paxar@gmail.com>
2022-05-19 18:13:36 +0300

    some fix

--- src/lab_06/lab_06_1.cpp
+++ src/lab_06/lab_06_1.cpp
@@ -10,58 +10,44 @@
 #include <cstdlib>
 #include <cstring>
 
+bool flag = false;
 sem_t* write_sem;
 sem_t* read_sem;
 int shmid;
 char* addr;
-const char* shm_name = "/shmem";
 
-static void* proc(void* arg) {
+static void* proc(void*) {
     printf("Thread 1 started\n");
-    auto* flag = reinterpret_cast<bool*>(arg);
     char buf[256];
-    int sem_status;
-    int failure = 0;
 
-    while (!(*flag)) {
-        sem_getvalue(write_sem, &sem_status);
-        if (sem_status == 0) {
-            failure = 0;
-            sem_wait(read_sem);
-
-            memset(buf, 0, 256);
-            long size = sysconf(_SC_NGROUPS_MAX);
-            unsigned usize = 0;
-            if (errno == -1) {
-                perror("Sysconf");
-            } else {
-                usize = static_cast<unsigned>(size);
-            }
-            auto* list = new gid_t[usize];
-            int count = getgroups(static_cast<int>(usize), list);
-            char idx_buf[10];
-            for (int idx = 0; idx < count; ++idx) {
-                std::sprintf(idx_buf, "%i", list[idx]);
-                strcat(buf, idx_buf);
-                if (idx < count - 1) {
-                    strcat(buf, " ");
-                }
-            }
-
-            std::sprintf(addr, "%s", buf);
-            printf("Data write\n");
-            printf("%s\n", addr);
-
-            sem_post(read_sem);
-            sem_post(write_sem);
-        } else {
-            sleep(1);
-            failure++;
-            if (failure == 10) {
-                printf("\nTime to write is over\n");
-                break;
+    while (!flag) {
+        memset(buf, 0, 256);
+        long size = sysconf(_SC_NGROUPS_MAX);
+        unsigned usize = 0;
+        if (errno == -1) {
+            perror("Sysconf");
+        }
+        else {
+            usize = static_cast<unsigned>(size);
+        }
+        auto* list = new gid_t[usize];
+        int count = getgroups(static_cast<int>(usize), list);
+        char idx_buf[10];
+        for (int idx = 0; idx < count; ++idx) {
+            std::sprintf(idx_buf, "%i", list[idx]);
+            strcat(buf, idx_buf);
+            if (idx < count - 1) {
+                strcat(buf, " ");
             }
         }
+
+        std::sprintf(addr, "%s", buf);
+        printf("Data write\n");
+        printf("%s\n", addr);
+
+        sem_post(write_sem);
+        sem_wait(read_sem);
+
         sleep(1);
     }
     pthread_exit(reinterpret_cast<void*>(0));
@@ -69,10 +55,12 @@ static void* proc(void* arg) {
 
 void sig_handler(int s) {
     munmap(addr, 256);
+    sem_close(write_sem);
     sem_unlink("/write_sem");
+    sem_close(read_sem);
     sem_unlink("/read_sem");
     close(shmid);
-    shm_unlink(shm_name);
+    shm_unlink("/shmem");
 
     if (s == SIGINT) {
         exit(1);
@@ -82,19 +70,18 @@ void sig_handler(int s) {
 int main() {
     printf("Program started\n");
 
-    bool flag = false;
     pthread_t id;
     void* exitcode;
 
-    write_sem = sem_open("/write_sem", O_CREAT, 0644, 0);
-    read_sem = sem_open("/read_sem", O_CREAT, 0644, 1);
-    shmid = shm_open(shm_name, O_CREAT | O_RDWR, 0644);
+    shmid = shm_open("/shmem", O_CREAT | O_RDWR, 0644);
     ftruncate(shmid, 256);
     addr = (char*)mmap(nullptr, 256, PROT_READ | PROT_WRITE, MAP_SHARED, shmid, 0);
+    write_sem = sem_open("/write_sem", O_CREAT, 0644, 0);
+    read_sem = sem_open("/read_sem", O_CREAT, 0644, 1);
 
     signal(SIGINT, sig_handler);
 
-    pthread_create(&id, nullptr, proc, &flag);
+    pthread_create(&id, nullptr, proc, nullptr);
     printf("Program is waiting for a keystroke\n");
     getchar();
     printf("Key pressed\n");
--- src/lab_06/lab_06_2.cpp
+++ src/lab_06/lab_06_2.cpp
@@ -8,37 +8,23 @@
 #include <cstdio>
 #include <cstdlib>
 
+bool flag = false;
 sem_t* write_sem;
 sem_t* read_sem;
 int shmid;
 char* addr;
-const char* shm_name = "/shmem";
 
-static void* proc(void* arg) {
+static void* proc(void*) {
     printf("Thread 2 started\n");
-    auto* flag = reinterpret_cast<bool*>(arg);
-    int sem_status;
-    int failure = 0;
 
-    while (!(*flag)) {
-        sem_getvalue(write_sem, &sem_status);
-        if (sem_status == 1) {
-            failure = 0;
-            sem_wait(read_sem);
+    while (!flag) {
+        sem_wait(write_sem);
 
-            printf("%s\n", addr);
-            printf("Data read\n");
+        printf("%s\n", addr);
+        printf("Data read\n");
 
-            sem_post(read_sem);
-            sem_wait(write_sem);
-        } else {
-            sleep(1);
-            failure++;
-            if (failure == 10) {
-                printf("\nTime to read is over\n");
-                break;
-            }
-        }
+        sem_post(read_sem);
+
         sleep(1);
     }
     pthread_exit(reinterpret_cast<void*>(0));
@@ -46,10 +32,12 @@ static void* proc(void* arg) {
 
 void sig_handler(int s) {
     munmap(addr, 256);
+    sem_close(write_sem);
     sem_unlink("/write_sem");
+    sem_close(read_sem);
     sem_unlink("/read_sem");
     close(shmid);
-    shm_unlink(shm_name);
+    shm_unlink("/shmem");
 
     if (s == SIGINT) {
         exit(1);
@@ -59,19 +47,18 @@ void sig_handler(int s) {
 int main() {
     printf("Program started\n");
 
-    bool flag = false;
     pthread_t id;
     void* exitcode;
 
-    write_sem = sem_open("/write_sem", O_CREAT, 0644, 0);
-    read_sem = sem_open("/read_sem", O_CREAT, 0644, 1);
-    shmid = shm_open(shm_name, O_CREAT | O_RDWR, 0644);
+    shmid = shm_open("/shmem", O_CREAT | O_RDWR, 0644);
     ftruncate(shmid, 256);
     addr = (char*)mmap(nullptr, 256, PROT_READ | PROT_WRITE, MAP_SHARED, shmid, 0);
+    write_sem = sem_open("/write_sem", O_CREAT, 0644, 0);
+    read_sem = sem_open("/read_sem", O_CREAT, 0644, 1);
 
     signal(SIGINT, sig_handler);
 
-    pthread_create(&id, nullptr, proc, &flag);
+    pthread_create(&id, nullptr, proc, nullptr);
     printf("Program is waiting for a keystroke\n");
     getchar();
     printf("Key pressed\n");
